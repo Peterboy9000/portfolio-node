13f7305bb58991ca413a6eec17e0634addbeaf38
Alen Mathew Thomas <alenthomas2021@gmail.com>
2026-03-08 13:45:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Supabase Client
 const supabase = createClient(
